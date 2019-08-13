6ab6d87e791c49748ad4219e707bbebf6f216e4e
Gabriel Okolie <gabbygabbylexy@gmail.com>
2019-08-13 15:30:35 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send('Hello');
+});
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`server started at ${port}...`));
+
